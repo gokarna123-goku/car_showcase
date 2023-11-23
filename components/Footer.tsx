16a7aad052eeb42bb7fe7d2e5899232d464b366b
Gokarna Adhikari <agokarna444@gmail.com>
2023-11-23 08:04:02 +0545

    working on the footer section

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
     return (
         <>
-            <div className="w-full">This is the footer section.</div>
+            <footer className="flex flex-col text-black-100 mt-5 border-t border-gray-100">
+                <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
+                    <div className="flex flex-col justify-start items-start gap-5">
+                        <Image src="/logo.svg" alt="logo" width={118} height={18} className="object-contain" />
+                    </div>
+                </div>
+            </footer>
         </>
     )
 }
