9aafec3a086644d1247764484722bc730f9a5bec
Gokarna Adhikari <agokarna444@gmail.com>
2023-11-23 08:44:07 +0545

    modified the hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import CustomButton from './CustomButton'
 
 const Hero = () => {
-    const handleScroll = () => {}
+    const handleScroll = () => { }
     return (
         <>
             <div className="hero">
@@ -14,15 +14,15 @@ const Hero = () => {
                         Streamline your car rental experience with our effortless booking process.
                     </p>
                     <CustomButton
-                    title="Explore Cars"
-                    containerStyles="bg-primary-blue text-white rounded-full mt-10"
-                    handleClick={handleScroll} />
+                        title="Explore Cars"
+                        containerStyles="bg-primary-blue text-white rounded-full mt-10"
+                        handleClick={handleScroll} />
                 </div>
                 <div className="hero__image-container">
                     <div className="hero__image">
                         <Image src="/hero.png" alt="hero" fill className="object-contain" />
-                        <div className="hero__image-overlay" />
                     </div>
+                    <div className="hero__image-overlay" />
                 </div>
             </div>
         </>
