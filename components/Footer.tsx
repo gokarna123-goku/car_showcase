4e9c30a8411918f18744a88541e0cac4d7dea929
Gokarna Adhikari <agokarna444@gmail.com>
2023-11-23 08:07:42 +0545

    working on the footer link through maping concepts

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,6 +8,12 @@ const Footer = () => {
                 <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
                     <div className="flex flex-col justify-start items-start gap-5">
                         <Image src="/logo.svg" alt="logo" width={118} height={18} className="object-contain" />
+                        <p className="texxt-base text-gray-700">
+                            Carhub 2023 <br /> All rights reserved &copy;
+                        </p>
+                    </div>
+                    <div className="footer__links">
+                        
                     </div>
                 </div>
             </footer>
