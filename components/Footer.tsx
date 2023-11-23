f43aebb16f141ef9f754d6ae56fd1ad99d739fb5
Gokarna Adhikari <agokarna444@gmail.com>
2023-11-23 08:35:33 +0545

    completed the footer section

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import { footerLinks } from "@/constants";
 
 const Footer = () => {
     return (
@@ -13,7 +14,23 @@ const Footer = () => {
                         </p>
                     </div>
                     <div className="footer__links">
-                        
+                        {footerLinks.map((link) => (
+                            <div key={link.title} className="footer__link">
+                                <h3 className="font-bold">{link.title}</h3>
+                                {link.links.map((item) => (
+                                    <Link key={item.title} href={item.url} className="text-gray-500">
+                                        {item.title}
+                                    </Link>
+                                ))}
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                <div className="flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10">
+                    <p>@2023 CarHub. All Rights Reserved</p>
+                    <div className="footer__copyrights-link">
+                        <Link href="/" className="text-gray-500">Privacy Policy</Link>
+                        <Link href="/" className="text-gray-500">Terms of Use</Link>
                     </div>
                 </div>
             </footer>
