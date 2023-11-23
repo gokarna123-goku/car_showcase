7b8f58236a437ac5bf1e61aacee890b56f1264ee
Gokarna Adhikari <agokarna444@gmail.com>
2023-11-23 09:42:41 +0545

    introduced new ui library called headless ui powered by tailwindcss labs

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,12 +1,38 @@
-import { SearchManufacturerProps } from '@/types'
-import React from 'react'
+"use client";
+
+import { useState } from 'react';
+
+import { Combobox, Transition } from '@headlessui/react';
+
+import { SearchManufacturerProps } from '@/types';
+import Image from 'next/image';
 
 const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacturerProps) => {
-  return (
-    <div>
-      
-    </div>
-  )
+    const [query, setQuery] = useState('');
+    return (
+        <>
+            <div className="search-manufacturer">
+                <Combobox>
+                    <div className='relative w-full'>
+                        <Combobox.Button className="absolute top-[14px]">
+                            <Image src="/car-logo.svg"
+                                width={20}
+                                height={20}
+                                className='ml-4'
+                                alt="car logo"
+                            />
+                        </Combobox.Button>
+                        <Combobox.Input
+                            className="search-manufacturer__input"
+                            placeholder="Volkswagen"
+                            displayValue={(manufacturer: string) => manufacturer}
+                            onChange={(e) => setQuery(e.target.value)}
+                        />
+                    </div>
+                </Combobox>
+            </div>
+        </>
+    )
 }
 
 export default SearchManufacturer
