96d78c3aa8e5682108e930f6dfff5f1e026d168c
Gokarna Adhikari <agokarna444@gmail.com>
2023-11-27 09:52:50 +0545

    integrating the code

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import Image from 'next/image';
 import { CarProps } from '@/types';
 import CustomButton from './CustomButton';
+import { calculateCarRent } from '@/utils';
 
 interface CarCardProps {
     car: CarProps;
@@ -11,17 +12,29 @@ interface CarCardProps {
 
 const CarCard = ({ car }: CarCardProps) => {
     const { city_mpg, year, make, model, transmission, drive } = car;
+
+    const carRent = calculateCarRent(city_mpg, year);
     return (
         <>
             <div className="car-card group">
                 <div className="car-card__content">
                     <h2 className='car-card__content-title'>{make} {model}</h2>
                 </div>
-                <p>
-                    <span>
-                        Car Rent...
-                    </span>
+                <p className='flex mt-6 text-[32px] leading-[38px] font-extrabold'>
+                    <span className='self-start text-[14px] leading-[17px] font-semibold'>$</span>
+                    {carRent}
+                    <span className='self-end text-[14px] leading-[17px] font-medium'>/day</span>
                 </p>
+
+                <div className="relative w-full h-40 my-3 object-contain">
+                    <Image
+                        src="/hero.png"
+                        alt="car model"
+                        fill priority
+                        className='object-contain' />
+
+                </div>
+
             </div>
         </>
     )
