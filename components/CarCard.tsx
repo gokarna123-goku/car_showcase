3ccbd7d808532c2d6670495380faa8ab62846f1b
Gokarna Adhikari <agokarna444@gmail.com>
2023-11-27 10:08:37 +0545

    making the car card

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -16,7 +16,7 @@ const CarCard = ({ car }: CarCardProps) => {
     const carRent = calculateCarRent(city_mpg, year);
     return (
         <>
-            <div className="car-card group">
+            <div className="car-card group mb-5">
                 <div className="car-card__content">
                     <h2 className='car-card__content-title'>{make} {model}</h2>
                 </div>
@@ -33,6 +33,37 @@ const CarCard = ({ car }: CarCardProps) => {
                         fill priority
                         className='object-contain' />
 
+                </div>
+
+                <div className="relative flex w-full mt-2">
+                    <div className="flex group-hover:invisible w-full justify-between text-gray">
+                        <div className="fle flex-col justify-center items-center gap-2">
+                            <Image src="/steering-wheel.svg" width={20} height={20} alt="steering wheel" />
+                            <p className="text-[14px]">
+                                {transmission === 'a' ? 'Automatic' : 'Manual'}
+                            </p>
+                        </div>
+                        <div className="fle flex-col justify-center items-center gap-2">
+                            <Image src="/tire.svg" width={20} height={20} alt="Tire wheel" />
+                            <p className="text-[14px]">
+                                {drive.toUpperCase()}
+                            </p>
+                        </div>
+                        <div className="fle flex-col justify-center items-center gap-2">
+                            <Image src="/gas.svg" width={20} height={20} alt="gas" />
+                            <p className="text-[14px]">
+                                {city_mpg} MPG
+                            </p>
+                        </div>
+                    </div>
+                    <div className="car-card__btn-container">
+                        <CustomButton 
+                        title='View More'
+                        containerStyles='w-full py-[16px] rounded-full bg-primary-blue'
+                        textStyles="text-white text-[14px] leading-[17px] font-bold"
+                        rightIcon="/right-arrow.svg"
+                        />
+                    </div>
                 </div>
 
             </div>
